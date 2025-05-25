5575d89d963131675b436c3a82496ace246e7b0c
samruddhi <samruddhikhilari630@gmail.com>
2025-05-25 15:44:40 +0530

    most of the work

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function bodyScroller(){
     const scroll = new LocomotiveScroll({
-    el: document.querySelector(".data-scroll-container"),
+    el: document.querySelector("[data-scroll-container]"),
     smooth: true
 });
 }
@@ -8,7 +8,6 @@ function bodyScroller(){
 function loaderEffect() {
 
  var tl = gsap.timeline();
-
 
   tl.to("#yellow1",{
     y:"-300px",
@@ -53,4 +52,19 @@ function loaderEffect() {
   });
 
 }
+
 loaderEffect();
+
+document.querySelector("#page2").addEventListener("mouseenter",function(){
+    
+  var ele = document.querySelectorAll(".ele");
+  ele.forEach(function(e){
+    e.addEventListener("mouseenter",function(det){
+      document.querySelector("#page2").style.backgroundImage=`url('${det.target.getAttribute("data-img")}')`;
+    })
+  })
+});
+
+document.querySelector("#page2").addEventListener("mouseleave",function(){
+  document.querySelector("#page2").style.backgroundImage='url("https://i.pinimg.com/736x/0f/69/39/0f6939ca4e265db250f2598a4679f4b9.jpg")';  
+});
